09213a2230a1f6d849098aba1dcfd7b31d6bbb91
adityadattamallick <adityadatta30@gmail.com>
2025-06-11 21:57:34 +0600

    Solving the basics

---
+++ pattern_3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void pattern(int n);
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    pattern(n);
+
+    return 0;
+}
+
+void pattern(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            std::cout << j + 1;
+        }
+        std::cout << "\n";
+    }
+}
