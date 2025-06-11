57aa782de5f37b0916aa3829dc725afd48aefb8f
adityadattamallick <adityadatta30@gmail.com>
2025-06-11 22:02:42 +0600

    Added space in the last pattern program

--- pattern_3.cpp
+++ pattern_3.cpp
@@ -14,7 +14,7 @@ int main() {
 void pattern(int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j <= i; j++) {
-            std::cout << j + 1;
+            std::cout << j + 1 << " ";
         }
         std::cout << "\n";
     }
