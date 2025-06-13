389d566a9f1b8c41b44ff750d0a802db4599a914
adityadattamallick <adityadatta30@gmail.com>
2025-06-13 19:58:47 +0600

    Solving

---
+++ pattern_5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void pattern(int n);
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    pattern(n);
+
+    return 0;
+}
+
+void pattern(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < n - i + 1; j++) {
+            std::cout << "* ";
+        }
+        std::cout << "\n";
+    }
+}
